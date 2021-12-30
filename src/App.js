056d365c549b55a848e818a0c0b3e67c72046f42
Franco Direnzo <fm.direnzo@gmail.com>
2021-12-30 12:27:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { HeaderApp } from "./components/HeaderApp";
+import { ListProducts } from "./components/Products/ListProducts";
+import { AlertProduct } from "./components/Products/AlertProduct";
+
+const App = () => {
+  return (
+    <>
+      <HeaderApp />
+      <Container>
+        <div className="App">
+          <AlertProduct />
+          <ListProducts />
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default App;
