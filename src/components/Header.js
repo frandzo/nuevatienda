6b05c9003388b2f0a3453a65cd5ccff0b80d950e
Franco Direnzo <fm.direnzo@gmail.com>
2021-12-30 16:33:16 -0300

    agregado carrito (no funcional)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import { Dropdown } from 'bootstrap';
-
 require('bootstrap');
 
 export function Header(){
     return (
-    <div>
+    <div className='nav'>
         <h1>MI TIENDA ONLINE</h1>
+        <div class="menu">
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox">
+          <img src='https://www.svgrepo.com/show/80543/shopping-cart-outline.svg' width="32px"/>
+        </label>
+        <ul>
+          <li>Elemento1</li>
+          <li>Elemento2</li>
+          <li>Elemento3</li>
+          <li>Elemento4</li>
+          <h4>Total</h4>
+        </ul>
+      </div>
     </div>
     )
 }
