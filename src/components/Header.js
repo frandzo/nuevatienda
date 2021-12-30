d3e746afb22bb14c881da58e142864ed63f4d360
Franco Direnzo <fm.direnzo@gmail.com>
2021-12-30 14:45:24 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,11 @@
-import React from "react";
-import { Navbar, Container } from "react-bootstrap";
-import logo from '../logo.svg'
-import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
+import { Dropdown } from 'bootstrap';
 
-const style = {
-  marginBottom: '30px'
+require('bootstrap');
+
+export function Header(){
+    return (
+    <div>
+        <h1>MI TIENDA ONLINE</h1>
+    </div>
+    )
 }
-
-export const Header = () => (
-  <Navbar bg="dark" variant="dark" style={style}>
-    <Container>
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      Mini Ecommerce
-      </Navbar.Brand>
-      <ShoppingCart />
-    </Container>
-  </Navbar>
-);
