cac924de9bc5ebb9f7671371ff280afaba301996
frandzo <frandzo@users.noreply.github.com>
2021-12-30 21:30:29 -0300

    Added delete from kart feature (total price not updated after delete *bug)

--- src/components/ShoppingCart/ShoppingCartItem.js
+++ src/components/ShoppingCart/ShoppingCartItem.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavDropdown } from "react-bootstrap";
+import db from "../../app/db/db";
 
 export const ShoppingCartItem = ({item}) => (
-  <li>{item.title} - {item.category} - ${item.price} </li>
+  <li onClick={()=>{db.cart.delete(item.id)}}>
+    {item.title} - {item.category} - ${item.price} 
+  </li>
 );
